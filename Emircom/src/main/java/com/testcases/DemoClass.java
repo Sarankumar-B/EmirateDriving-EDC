3949c211c45d4aaf76ffa5bafe790a8bb4599279
ponraj.kasirajan <ponraj.kasirajan@Ponraj.Impigertech.com>
2023-09-18 14:23:45 +0530

    Code Changes by ponraj

--- Emircom/src/main/java/com/testcases/DemoClass.java
+++ Emircom/src/main/java/com/testcases/DemoClass.java
@@ -2,44 +2,10 @@ package com.testcases;
 
 public class DemoClass extends BaseClass {
 
-	
-
-	     
-	      
-
-		    private int age;
-			
-			private String name;
-			
-			private long ph;
-
-			public int getAge() {
-				return age;
-			}
-
-			public void setAge(int age) {
-				this.age = age;
-			}
-
-			public String getName() {
-				return name;
-			}
-
-			public void setName(String name) {
-				this.name = name;
-			}
-
-			public long getPh() {
-				return ph;
-			}
-
-			public void setPh(long ph) {
-				this.ph = ph;
-		
+	//CodeChangesby ponraj
 	     
 			
 		
  
 	  }
-}
 
