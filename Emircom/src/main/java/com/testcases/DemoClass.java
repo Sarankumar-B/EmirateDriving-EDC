0005c0430da1b00a2f25d7f48a7ba69f68778c88
RANJITHKUMAR1707 <ranjithkumar.sivakum@Ranjith-290B2G2.Impigertech.com>
2023-09-28 12:45:40 +0530

    latest Commit

--- Emircom/src/main/java/com/testcases/DemoClass.java
+++ Emircom/src/main/java/com/testcases/DemoClass.java
@@ -1,24 +1,12 @@
 package com.testcases;
 
-import org.testng.annotations.Test;
-
 public class DemoClass extends BaseClass {
 
-	@Test(priority = 0)
-	private void ranji() {
-		System.out.println("ranji");
-
+	public static void main(String[] args) {
+		
+		String x = "vsd\"tfsatfd\"jcvahkbv";
+System.out.println(x);
+		
+		}
 	}
 	
-	@Test(priority = 1)
-	private void barath() {
-		System.out.println("barath");
-
-	}
-	
-	@Test(priority = 2)
-	private void raja() {
-		System.out.println("raja");
-	}
-	
-}
